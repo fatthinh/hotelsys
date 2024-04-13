dcb11ce4eab0130c3be025da50ea2b7621305b99
fatthinh <pthinh.lama@gmail.com>
2024-04-13 20:27:59 +0700

    update

--- hotelapp/__init__.py
+++ hotelapp/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 from urllib.parse import quote
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./templates', static_folder='./static')
+
 app.secret_key = '890f32ff363679f635988bd8c7910afe41fb463937feafb39df5490489c4c171'
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@localhost/bookingdb?charset=utf8mb4" % quote(
     '0335037042Think.')
