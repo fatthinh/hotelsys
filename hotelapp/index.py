2823491b302dfe552bd2c1a3fe7d7905965d34b0
fatthinh <pthinh.lama@gmail.com>
2024-04-15 11:34:47 +0700

    update

--- hotelapp/index.py
+++ hotelapp/index.py
@@ -8,6 +8,16 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/rooms')
+def rooms():
+    return render_template('rooms.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
 if __name__ == "__main__":
     with app.app_context():
         app.run(debug=True)
